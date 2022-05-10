3f69c528b9fb1552cfb9cba16e1187617b8dc030
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2022-05-10 18:43:43 -0300

    Update design4.vhd

--- design4.vhd
+++ design4.vhd
@@ -1,47 +1,59 @@
 library ieee;
+
 use ieee.std_logic_1164.all;
 
-entity somador1b is 
+entity ula is
 	port (a : in std_logic;
     	  b : in std_logic;
-          modo : in std_logic;
+          carryIn : in std_logic;
+          sel : in std_logic;
+          less : in std_logic;
+          bInvert : in std_logic;
+          carryOut : out std_logic;
+          saida : out std_logic);
+end ula;
+
+architecture ula_arch of ula is
+	component somador1b is
+    	port(a : in std_logic;
+    	  b : in std_logic;
+          carryIn : out std_logic;
           s1 : out std_logic;
           s2 : out std_logic;
-          s3 : out std_logic);
-end somador1b;
-
-architecture somador1b_arch of somador1b is
-    component meio_somador is
-	port( a : in std_logic;
-    	  b : in std_logic;
-          s1 : out std_logic;
-          carry : out std_logic );
-	end component meio_somador;
+          s3 : out std_logic;
+          carryOut : out std_logic);
+     end component;
      
-     signal S_primeira_soma : std_logic;
-     signal S_primeiro_carry : std_logic;
-     signal S_segundo_carry : std_logic;
-     signal S_sel_modo : std_logic;
+     component mult3x1 is
+     	port (e1, e2, e3 in std_logic; 
+  		sel: in std_logic_vector(1 downto 0);
+		s: out std_logic);
+     end component;
      
+     signal S_signal4 : std_logic;
+     signal S_e1 : std_logic;
+     signal S_e2 : std_logic;
+     signal S_e3 : std_logic;
+ 	 
      begin 
-     S_sel_modo <= B xor modo;
-     S_primeiro_carry <= modo;
-     
-     	somador1 : meio_somador
-        	port map (a => a, 
-            		  b => S_sel_modo,
-                      s1 => S_primeira_soma,
-                      carry => S_primeiro_carry);
-           
-        somador2 : meio_somador
-        	port map (a => S_primeira_soma, 
-            		  b => S_primeiro_carry,
-                      s1 => s1,
-                      carry => S_segundo_carry);
-                      
-         carry <= S_primeiro_carry or S_segundo_carry;
-         
-         s2 <= a and b;
-         s3 <= a or b;
-         
-end somador1b_arch;
+     with bInvert select
+
+  		S_signal4 <= b when '0',
+       				(not b) when others;
+       
+     somador : somador1b
+     	port map(a => a, 
+        		 b => S_signal4,
+                 carryIn => carryIn,
+                 s1 => S_e1,
+                 s2 => S_e2,
+                 s3 => S_e3,
+                 carryOut => carryOut);
+      mux : mult3x1
+     	port map(e1 => S_e1,
+        	  e2 => S_e2,
+              e3 => S_e3,
+              sel => sel,
+              s => saida);
+      
+end ula_arch;
