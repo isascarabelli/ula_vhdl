149a2469b43b95b439797fda36f6818dde64bcb6
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2022-05-10 18:03:38 -0300

    Update design4.vhd

--- design4.vhd
+++ design4.vhd
@@ -1,45 +1,47 @@
 library ieee;
-
 use ieee.std_logic_1164.all;
 
-entity ula is
+entity somador1b is 
 	port (a : in std_logic;
     	  b : in std_logic;
-          c : in std_logic;
-          sel : in std_logic;
-          carryOut : out std_logic;
-          saida : out std_logic);
-end ula;
+          modo : in std_logic;
+          s1 : out std_logic;
+          s2 : out std_logic;
+          s3 : out std_logic);
+end somador1b;
 
-architecture ula_arch of ula is
-	component somador1b is
-    	port(a : in std_logic;
-             b : in std_logic;
-             modo : in std_logic;
-             s1 : out std_logic;
-             carry : out std_logic);
-     end component;
+architecture somador1b_arch of somador1b is
+    component meio_somador is
+	port( a : in std_logic;
+    	  b : in std_logic;
+          s1 : out std_logic;
+          carry : out std_logic );
+	end component meio_somador;
      
-     component mult4x1 is
-     	port (e1, e2, e3 in std_logic; 
-  		sel: in std_logic_vector(1 downto 0);
-		s: out std_logic);
-     end component;
+     signal S_primeira_soma : std_logic;
+     signal S_primeiro_carry : std_logic;
+     signal S_segundo_carry : std_logic;
+     signal S_sel_modo : std_logic;
      
-     signal S_signal4 : std_logic;
- 	 
      begin 
-     somador : somador1b
-     	port map(a => a, 
-        		 b => b,
-                 modo => c,
-                 s1 => S_signal4,
-                 carry => carryOut);
-      mux : mult3x1
-     	port map(e1 => S_signal4,
-        	  e2 => s2,
-              e3 => s3,
-              sel => sel,
-              s => saida);
-      
-end ula_arch;
+     S_sel_modo <= B xor modo;
+     S_primeiro_carry <= modo;
+     
+     	somador1 : meio_somador
+        	port map (a => a, 
+            		  b => S_sel_modo,
+                      s1 => S_primeira_soma,
+                      carry => S_primeiro_carry);
+           
+        somador2 : meio_somador
+        	port map (a => S_primeira_soma, 
+            		  b => S_primeiro_carry,
+                      s1 => s1,
+                      carry => S_segundo_carry);
+                      
+         carry <= S_primeiro_carry or S_segundo_carry;
+         
+         s2 <= a and b;
+         s3 <= a or b;
+         
+end somador1b_arch;
