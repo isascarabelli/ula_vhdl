aec48d99575031f4e4e2760310877702a8e8a875
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2022-05-10 17:58:33 -0300

    Update design3.vhd

--- design3.vhd
+++ design3.vhd
@@ -2,15 +2,15 @@ library ieee;
 
 use ieee.std_logic_1164.all;
 
-entity mult4x1 is
+entity mult3x1 is
 
-  port (e1, e2, e3, e4 in std_logic; 
+  port (e1, e2, e3 in std_logic; 
   		sel: in std_logic_vector(1 downto 0);
 		s: out std_logic);
 
-end mult4x1;
+end mult3x1;
 
-architecture arch_mult4x1 of mult4x1 is
+architecture arch_mult3x1 of mult3x1 is
 
 begin
 
@@ -18,7 +18,6 @@ begin
 
   s <= e1 when '00',
        e2 when '01',
-       e3 when '10',
-       e4 when others;
+       e3 when others;
 
-end arch_mult4x1;
+end arch_mult3x1;
