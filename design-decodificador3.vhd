ba9983003414880d120b8d92debb632592a0c701
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2022-05-16 11:33:02 -0300

    Update design-decodificador3.vhd

--- design-decodificador3.vhd
+++ design-decodificador3.vhd
@@ -4,13 +4,20 @@ use ieee.std_logic_1164.all;
 
 entity decodificador is
 
-    port (a1, b1, c1: in std_logic;
+    port (opcode: in std_logic_vector(3 downto 0);
           sSel1, sSel2, sSel3: out std_logic);
           
 end decodificador;
 
 architecture archDecode of decodificador is
+    signal a1: in std_logic;
+    signal b1: in std_logic;
+    signal c1: in std_logic;
+
 begin
+    a1 <= opcode(0);
+    c1 <= opcode(1);
+    b1 <= opcode(2);
 
     sSel1 <= (b1 or c1) and (a1 or b1);
     sSel2 <= b1;
